a53fe39ff99f7b7cb6e21ac1246f9b02f2cb9fc0
baldwinvs <baldwinvs@outlook.com>
2019-10-15 22:49:55 -0700

    base commit for assignment02

---
+++ assignment02/assignment02_iar/main.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+int main()
+{
+    int32_t counter = 0;
+
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+
+    return 0;
+}
